fb9510f48d1160ca9a239e4ace25058ec9aca034
Vadym24 <vadykpopovych2@gmail.com>
2018-03-18 19:18:34 +0200

    з нуля

---
+++ test/MainTest.java
@@ -0,0 +1,53 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void isDelim() {
+
+    }
+
+    @Test
+    public void isOperator() {
+
+    }
+
+    @Test
+    public void priority() {
+    }
+
+    @Test
+    public void processOperator() {
+    }
+
+    @Test
+    public void eval() {
+
+    }
+
+    @Test
+    public void livesumTest(){
+    Main main = new Main();
+    Assert.assertEquals("Sum = " , 0 , main.livesum() );
+    }
+
+    @Test
+    public void mathematic() {
+        
+    }
+
+    @Test
+    public void vkl() {
+    }
+
+    @Test
+    public void vykl() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
