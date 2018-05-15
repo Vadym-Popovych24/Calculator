ca26e749cbb22f78d98cf7b4f25986d23d1510ea
Vadym24 <vadykpopovych2@gmail.com>
2018-05-15 18:28:43 +0300

    -1 test

--- test/ProcessTest.java
+++ test/ProcessTest.java
@@ -7,19 +7,19 @@ import java.util.LinkedList;
 
 import static org.junit.Assert.*;
 
-public class ProcessTest  {
-   Process pro = new Process();
+public class ProcessTest {
+    Process pro = new Process();
     Main main = new Main();
-        @Test /*expected = ArithmeticException.class)*/
-        public void test_division_by_zero() {
-        double t2=pro.t;
-        double l2=pro.l;
 
-        int an=(int)(t2/0);
-        Assert.assertEquals(an,0);
-        }
+    @Test /*expected = ArithmeticException.class)*/ public void test_division_by_zero() {
+        double t2 = pro.t;
+        double l2 = pro.l;
 
-    @Test
+        double an = (t2 / 0);
+        Assert.assertEquals(an, 0);
+    }
+}
+  /*  @Test
     public void test_division() {
             pro.t =5;
             pro.l =2;
@@ -30,4 +30,4 @@ public class ProcessTest  {
         Assert.assertEquals("Division = ",main.mn, 2.5,0);
     }
 
-}
+}*/
